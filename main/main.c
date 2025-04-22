00e4dd514e5de1d665e583be67c1f04ceac02b0a
Vitor Katakura <7vitorgomez@gmail.com>
2025-04-22 15:40:59 -0300

    finalizado

--- main/main.c
+++ main/main.c
@@ -12,10 +12,22 @@
 
 #include "Fusion.h"
 #define SAMPLE_PERIOD (0.01f) // replace this with actual sample period
+#define UART_ID uart0
+#define BAUD_RATE 115200
+
+#define UART_TX_PIN 0
+#define UART_RX_PIN 1
+
+typedef struct pos {
+    int axis;
+    float val;
+} pos_t;
 
 const int MPU_ADDRESS = 0x68;
 const int I2C_SDA_GPIO = 4;
 const int I2C_SCL_GPIO = 5;
+
+QueueHandle_t xQueuePos;
 
 static void mpu6050_reset() {
     // Two byte reset. First byte register, second byte data
@@ -44,17 +56,18 @@ static void mpu6050_read_raw(int16_t accel[3], int16_t gyro[3], int16_t *temp) {
     // The register is auto incrementing on each read
     val = 0x43;
     i2c_write_blocking(i2c_default, MPU_ADDRESS, &val, 1, true);
-    i2c_read_blocking(i2c_default, MPU_ADDRESS, buffer, 6, false);  // False - finished with bus
+    i2c_read_blocking(i2c_default, MPU_ADDRESS, buffer, 6, false); // False - finished with bus
 
     for (int i = 0; i < 3; i++) {
-        gyro[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);;
+        gyro[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);
+        ;
     }
 
     // Now temperature from reg 0x41 for 2 bytes
     // The register is auto incrementing on each read
     val = 0x41;
     i2c_write_blocking(i2c_default, MPU_ADDRESS, &val, 1, true);
-    i2c_read_blocking(i2c_default, MPU_ADDRESS, buffer, 2, false);  // False - finished with bus
+    i2c_read_blocking(i2c_default, MPU_ADDRESS, buffer, 2, false); // False - finished with bus
 
     *temp = buffer[0] << 8 | buffer[1];
 }
@@ -70,21 +83,85 @@ void mpu6050_task(void *p) {
     mpu6050_reset();
     int16_t acceleration[3], gyro[3], temp;
 
-    while(1) {
-        // leitura da MPU, sem fusao de dados
+    FusionAhrs ahrs;
+    FusionAhrsInitialise(&ahrs);
+
+    while (1) {
+        pos_t pos_data;
+
+        // leitura da MPU, com fusão de dados
         mpu6050_read_raw(acceleration, gyro, &temp);
-        printf("Acc. X = %d, Y = %d, Z = %d\n", acceleration[0], acceleration[1], acceleration[2]);
-        printf("Gyro. X = %d, Y = %d, Z = %d\n", gyro[0], gyro[1], gyro[2]);
-        printf("Temp. = %f\n", (temp / 340.0) + 36.53);
+        FusionVector gyroscope = {
+            .axis.x = gyro[0] / 131.0f, // Conversão para graus/s
+            .axis.y = gyro[1] / 131.0f,
+            .axis.z = gyro[2] / 131.0f,
+        };
+
+        FusionVector accelerometer = {
+            .axis.x = acceleration[0] / 16384.0f, // Conversão para g
+            .axis.y = acceleration[1] / 16384.0f,
+            .axis.z = acceleration[2] / 16384.0f,
+        };
+
+        FusionAhrsUpdateNoMagnetometer(&ahrs, gyroscope, accelerometer, SAMPLE_PERIOD);
+
+        const FusionEuler euler = FusionQuaternionToEuler(FusionAhrsGetQuaternion(&ahrs));
+
+        // printf("Acc. X = %d, Y = %d, Z = %d\n", acceleration[0], acceleration[1], acceleration[2]);
+        // printf("Roll %0.1f, Pitch %0.1f, Yaw %0.1f\n", euler.angle.roll, euler.angle.pitch, euler.angle.yaw);
+        // printf("Temp. = %f\n", (temp / 340.0) + 36.53);
+
+        if (acceleration[1] < -25000) {
+            pos_data.axis = 2;
+            pos_data.val = 0.0f;
+            xQueueSend(xQueuePos, &pos_data, 0);
+        }
+
+        if (euler.angle.yaw != 0) {
+            pos_data.axis = 0;
+            pos_data.val = euler.angle.yaw;
+            xQueueSend(xQueuePos, &pos_data, 0);
+        }
+
+        if (euler.angle.roll != 0) {
+            pos_data.axis = 1;
+            pos_data.val = euler.angle.roll;
+            xQueueSend(xQueuePos, &pos_data, 0);
+        }
 
         vTaskDelay(pdMS_TO_TICKS(10));
     }
 }
 
+void uart_task(void *p) {
+    pos_t data;
+    while (true) {
+        if (xQueueReceive(xQueuePos, &data, 1e6)) {
+            uint8_t axis = (uint8_t)data.axis;
+            uint16_t val = -1 * (uint16_t)((int)data.val & 0xFFFF);
+            uint8_t lsb = val & 0xFF;
+            uint8_t msb = (val >> 8) & 0xFF;
+            uint8_t end = 0xFF;
+
+            uint8_t pacote[4] = {axis, lsb, msb, end};
+            uart_write_blocking(UART_ID, pacote, 4);
+        }
+    }
+}
+
 int main() {
     stdio_init_all();
+    uart_init(uart0, BAUD_RATE);
+    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+
+    xQueuePos = xQueueCreate(32, sizeof(pos_t));
+
+    if (xQueuePos == NULL)
+        printf("falha em criar a fila \n");
 
     xTaskCreate(mpu6050_task, "mpu6050_Task 1", 8192, NULL, 1, NULL);
+    xTaskCreate(uart_task, "UART Task", 4095, NULL, 1, NULL);
 
     vTaskStartScheduler();
 
